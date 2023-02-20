68cfd2bdd7e2349423f2e5153682fb0540ae6af6
unknown <kamila.wilczynska92@gmail.com>
2023-02-20 21:39:26 +0000

    add single post

--- src/components/pages/Post/Post.js
+++ src/components/pages/Post/Post.js
@@ -1,9 +1,43 @@
-function Home() {
+import { Container } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { Button, Row, Col } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { getPostById } from "../../../redux/postsRedux";
+import { Link } from "react-router-dom";
+
+function Post() {
+	const { id } = useParams();
+	const postInformation = useSelector((state) => getPostById(state, id))
 	return (
-	  <div>
-		<h1>Single post</h1>
-	  </div>
+		<Container>
+			<Row className="justify-content-center">
+			<Col md={8} className="d-flex justify-content-between">
+				<h1>{postInformation.title} </h1>
+				</Col>
+				<Col md={4}>
+					<div>
+						<Link key={postInformation.id} to={"/post/edit/" + postInformation.id}>
+							<Button variant="outline-info" className="m-2">
+								Edit
+							</Button>
+						</Link>
+						<Button variant="outline-danger">Delete</Button>
+					</div>
+				</Col>
+			</Row>
+			<Row>
+				<Col md="auto">
+					<p><b>Author:</b>
+						{postInformation.author} </p>
+
+					<p><b>Published: </b>
+						{postInformation.publishedDate} </p>
+						{postInformation.content}
+				</Col>
+			</Row>
+
+		</Container>
 	);
-  }
-  
-  export default Home;
+}
+
+export default Post;
--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -1,8 +1,11 @@
 //selectors
 
 export const getAllPosts = (state) => state.posts;
+
+export const getPostById = ({ posts }, id) => posts.find((post) => post.id === id); //search for the post from the store
+
 // actions
-const createActionName = actionName => `app/posts/${actionName}`;
+const createActionName = (actionName) => `app/posts/${actionName}`;
 
 // action creators
 const postsReducer = (statePart = [], action) => {
