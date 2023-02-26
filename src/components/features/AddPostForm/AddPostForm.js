4608e812d11318c9b7e70b753ffb7fff2f8c2086
unknown <kamila.wilczynska92@gmail.com>
2023-02-26 18:02:56 +0000

    Add states to Form

--- src/components/features/AddPostForm/AddPostForm.js
+++ src/components/features/AddPostForm/AddPostForm.js
@@ -1,50 +1,77 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { Row, Col } from 'react-bootstrap'; 
+import { Row, Col } from 'react-bootstrap';
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
-function AddPostForm() {
-  return (
-	<>
-	<Row className="justify-content-md-center">
-	<Col xs={12} md={10} >
-	<Form>
-	<Col md={6}>
-    
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Title</Form.Label>
-        <Form.Control type="email" placeholder="Enter title" />
-      </Form.Group>
+function AddPostForm(props) {
 
-	  <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Author</Form.Label>
-        <Form.Control type="email" placeholder="Enter author" />   
-      </Form.Group>
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Published</Form.Label>
-        <Form.Control type="password" placeholder="Enter date" />
-      </Form.Group>
-     </Col> 
+	const [title, setTitle] = useState("");
+	const [author, setAuthor] = useState("");
+	const [published, setPublished] = useState("");
+	const [shortDescription, setshortDescription] = useState("");
+	const [content, setContent] = useState("");
 
-	  <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Short description</Form.Label>
-        <Form.Control type="password" placeholder="Enter description" />
-      </Form.Group>
-      
+	return (
+		<>
+			<Row className="justify-content-md-center">
+				<Col xs={12} md={10} >
+					<Form>
+						<Col md={6}>
 
-	  <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Main Content</Form.Label>
-        <Form.Control type="password" placeholder="Enter content" />
-      </Form.Group>
-     
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-	</Col>
-	</Row>
-	</>
-  );
+							<Form.Group className="mb-3" controlId="title">
+								<Form.Label>Title</Form.Label>
+								<Form.Control type="email" placeholder="Enter title" 
+								onChange={(e) => setTitle(e.target.value)}/>
+							</Form.Group>
+
+							<Form.Group className="mb-3" controlId="Author">
+								<Form.Label>Author</Form.Label>
+								<Form.Control type="email" placeholder="Enter author" 
+								onChange={(e) => setAuthor(e.target.value)}/>
+							</Form.Group>
+
+							<Form.Group className="mb-3" controlId="formBasicPassword">
+								<Form.Label>Published</Form.Label>
+								<Form.Control type="password" placeholder="Enter date" 
+								onChange={(e) => setPublished(e.target.value)}/>
+							</Form.Group>
+						</Col>
+
+						<Form.Group className="mb-3" controlId="descriptionForm">
+							<Form.Label>Short description</Form.Label>
+							<Form.Control
+								type="text"
+								as="textarea"
+								rows={3}
+								placeholder="Enter description" 
+								onChange={(e) => setshortDescription(e.target.value)}/>
+						</Form.Group>
+
+
+						<Form.Group className="mb-3" controlId="ContentForm">
+							<Form.Label>Main Content</Form.Label>
+							<Form.Control type="text"
+								as="textarea"
+								rows={6}
+								placeholder="Enter content" 
+								onChange={(e) => setContent(e.target.value)}
+								/>
+						</Form.Group>
+						
+						<Button variant="primary" type="submit">
+							Add Post
+						</Button>
+						
+					</Form>
+				</Col>
+			</Row>
+		</>
+	);
 }
 
 
